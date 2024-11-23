6a0d8852cc2badd5535da67edc3bfac8492d5464
pifragile <pigu@bluewin.ch>
2024-11-23 21:09:29 +0800

    headers

--- src/app/api/email-13377331/route.jsx
+++ src/app/api/email-13377331/route.jsx
@@ -2,6 +2,19 @@ import { sendEmail } from '@/lib/mail'
 import { NextResponse } from 'next/server'
 
 export async function POST(req) {
+  const headers = new Headers()
+  headers.set('Access-Control-Allow-Origin', '*') // Replace '*' with your origin
+  headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  // Handle preflight OPTIONS requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204, // No content
+      headers,
+    })
+  }
+
   try {
     const data = JSON.stringify(await req.json())
     const emailConfig = {
@@ -18,9 +31,19 @@ export async function POST(req) {
     // Send the email
     await sendEmail(emailConfig)
 
-    return NextResponse.json({ message: 'Success' })
+    return NextResponse.json(
+      { message: 'Success' },
+      {
+        headers,
+      },
+    )
   } catch (err) {
     console.log(err)
-    return NextResponse.json({ message: 'Internal server error', status: 500 })
+    return NextResponse.json(
+      { message: 'Internal server error', status: 500 },
+      {
+        headers,
+      },
+    )
   }
 }

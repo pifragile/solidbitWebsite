e7237c7cf88b8276f463cbeb632de142da1aabdb
pifragile <pigu@bluewin.ch>
2025-06-30 13:10:31 +0800

    scale fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,7 @@ export default function Layout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
         <MainComponent/>
